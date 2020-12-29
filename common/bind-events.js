24f220ed2104f58395714485477254b4a5fbe7b1
jhsm croonen <info@jcroonen.nl>
2020-12-29 12:33:54 +0100

    test initial deploy data

--- common/bind-events.js
+++ common/bind-events.js
@@ -2,7 +2,8 @@ function bindToElements(type, bindings) {
     for (const [id, listener] of bindings) {
         const element = document.getElementById(id);
         if (!element) {
-            console.error('id does not exist', id);
+            console.error('id does not exist: ', id);
+            console.trace();
         } else {
             element.addEventListener(type, listener);
         }
--- common/persist.js
+++ common/persist.js
@@ -23,21 +23,13 @@ function persistData(data) {
 function getInitialData(cb) {
     // cb(categories); // static data
     // get json
-    const urlToInitialData = '../initial_data.json';
+    const urlToInitialData = '../initial_data_deploy.json';
     fetch(urlToInitialData)
         .then(response => response.json())
         .then(categories => cb(categories));
 }
 
-function getDataFromStorage(cb) {
-    // showLocalStorage();
-
-    // test with static
-    // cb(categories); // static data
-    // return;
-
-    // getInitialData(data => console.log(data));
-
+function dataFromStorage(cb) {
     chrome.storage.local.get([key], result => {
         // console.log('Value currently is ', result[key]);
         if (result[key]) {
@@ -47,7 +39,20 @@ function getDataFromStorage(cb) {
             getInitialData(cb);
         }
     })
+}
 
+function getDataFromStorage(cb) {
+    // showLocalStorage();
+
+    // +++ test with static +++
+    const testInitial = true;
+    // +++ end test with static +++
+
+    if (testInitial) {
+        getInitialData(cb);
+    } else {
+        dataFromStorage(cb);
+    }
 }
 
 export {persistData, getDataFromStorage}
